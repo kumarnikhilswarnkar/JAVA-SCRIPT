c927de511c5a4d97cbf1aae68e66875914ebb01a
NIKHIL KUMAR <160383276+kumarnikhilswarnkar@users.noreply.github.com>
2024-12-18 13:31:30 +0000

    conversion

---
+++ .devcontainer/01_basics/Data type conversion.js
@@ -0,0 +1,14 @@
+let score=33
+console.log( typeof score);//before conversion
+let scoreconversion=String(score)
+console.log( typeof scoreconversion);//after conversion
+
+let marks="33"
+console.log( typeof marks);//before conversion
+let marksconversion=Number(score)
+console.log( typeof marksconversion);//after conversion
+
+let num="33abc"
+console.log( typeof num);//before conversion
+let numconversion=(score)
+console.log( typeof numconversion);//after conversion
