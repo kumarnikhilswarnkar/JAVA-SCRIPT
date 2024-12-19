0253b8ccd2b2554f910c2a5ce59ca71103a0f19f
NIKHIL KUMAR <160383276+kumarnikhilswarnkar@users.noreply.github.com>
2024-12-19 07:54:14 +0000

    DATA TYPES

---
+++ .devcontainer/01_basics/Datatype.js
@@ -0,0 +1,23 @@
+/* there are mainly two type of data type 
+1)primitive and 2)nin-primitive*/
+//primitive data type
+//NUMBER,STRING,BOOLEAN,NULL,UNDEFINE,SYMBOL AND BIGINT
+ let age=23
+ let name="nikhil"
+ let indian=true
+ let salary=null
+let girlfriend=undefined
+let num=123457679598696986489n //big int
+//const id=symbol('123')
+console.table([age,name,indian,salary,girlfriend,num])
+//NON PRIMITIVE 
+//ARRAY,OBJECT AND FUNCTIONS
+let heroes=["saktiman","balveer","superman"]//array
+let myobj={
+name="kriti"           //objects are in a bracket
+age=22
+}
+let myfunction=function()
+{
+    console.log(2+2)
+}
