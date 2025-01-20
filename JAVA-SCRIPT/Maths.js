071c3dc185705500f1487bb4ecc4d541e4b3f6c2
NIKHIL KUMAR <160383276+kumarnikhilswarnkar@users.noreply.github.com>
2025-01-20 03:22:49 +0000

    maths methods

---
+++ JAVA-SCRIPT/Maths.js
@@ -0,0 +1,9 @@
+console.log(Math.abs(-4));
+console.log(Math.round(2.9));
+console.log(Math.ceil(2.9));
+console.log(Math.floor(2.9));
+console.log(Math.max(14,28,39,333));
+console.log(Math.min(1,22,01,222));
+
+
+
