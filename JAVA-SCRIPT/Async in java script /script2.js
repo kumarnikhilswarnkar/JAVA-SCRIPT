6ed315dcdcc1ea846afa73fdceda84439cfe7769
NIKHIL KUMAR <160383276+kumarnikhilswarnkar@users.noreply.github.com>
2025-02-05 05:22:40 +0000

    Asynchronus  programming

---
+++ JAVA-SCRIPT/Async in java script /script2.js
@@ -0,0 +1,33 @@
+// function Even(){
+//     const a=10;
+//     const b=20;
+//     if(a%2==0 && b%2==0)
+//     {
+//         console.log("Numbers are even ");
+        
+//     }
+//     else{
+//         console.log("Numbers are not even");
+        
+//     }
+// }
+// setTimeout(Even,4000);
+
+console.log("Nikhil");
+console.log("kumar");
+
+setTimeout(()=>{
+    const a=10;
+    const b=20;
+    if(a%2==0 && b%2==0)
+    {
+        console.log("Numbers are even  sucess");
+        
+    }
+    else{
+        console.log("Numbers are not even");
+        
+    }
+
+},5000);
+console.log("Swarnkar");
